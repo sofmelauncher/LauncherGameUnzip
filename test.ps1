bcf30cba10f4f37fa7d57b4303b28042c0365e26
すー <aaaiiiuuu35@gmail.com>
2018-10-25 01:34:24 +0900

    add init

---
+++ test.ps1
@@ -0,0 +1,41 @@
+﻿function Log($text){
+    $time = (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss.fff')
+    Write-Output "[${time}] : ${text}";
+}
+
+function NoTimeLog($text){
+    Write-Output "${text}";
+}
+set LOG_PATH "./Expandlog.log" -option constant
+
+Start-Transcript -path $LOG_PATH -append;
+
+#$basedir = "C:\Users\SAVIO\Desktop\test";
+$basedir = (Convert-Path .);
+
+Log("ディレクトリ：${basedir}");
+$gomidir = $basedir + "\gomi";
+New-Item $gomidir -ItemType Directory -Force;
+Log("Search zip files...");
+$zipfiles = Get-ChildItem $basedir -Recurse | Where-Object {$_.Extension -eq ".zip"}
+    
+Log("Expand zip files...");
+foreach ($item in $zipfiles){
+    $destination = $item.FullName;
+    $destination = $destination.Substring(0, $destination.Length - ($item.Extension).Length);
+    Log("[Destination] ${destination}")
+    #$buffer = New-Item -Path $destination -ItemType Directory
+    Log("[Expand] ${item}");
+    #Expand-Archive -Path $item.FullName -DestinationPath $destination
+    Log("[Move-Item] ${item}");
+    #Move-Item $item.FullName $gomidir;
+}
+
+NoTimeLog("Zip List");
+foreach ($item in $zipfiles){
+    NoTimeLog("${item}");
+}
+
+
+Stop-Transcript
+
---
+++ test2.ps1
@@ -0,0 +1,31 @@
+﻿#function Log($text){
+    #$time = Get-Data;
+    #Write-Output "${time}${text}";
+#}
+
+set LOG_PATH "./Expandlog.log" -option constant
+
+Start-Transcript -path $LOG_PATH -append;
+
+#$basedir = "C:\Users\SAVIO\Desktop\test";
+$basedir = (Convert-Path .);
+
+
+$gomidir = $basedir + "\gomi";
+New-Item $gomidir -ItemType Directory -Force;
+Write-Host "Search zip files..."
+$zipfiles = Get-ChildItem $basedir -Recurse | Where-Object {$_.Extension -eq ".zip"}
+ 
+Write-Host "Expand zip files..."
+foreach ($item in $zipfiles){
+    Write-Host $item.Name -ForegroundColor Green
+
+    $destination = $item.FullName;
+    $destination = $destination.Substring(0, $destination.Length - ($item.Extension).Length);
+    $buffer = New-Item -Path $destination -ItemType Directory
+ 
+    Expand-Archive -Path $item.FullName -DestinationPath $destination
+    Move-Item $item.FullName $gomidir;
+}
+Stop-Transcript
+
