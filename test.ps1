2e9ed60a4349270fd337bdb873084aa3bcff6e05
すー <aaaiiiuuu35@gmail.com>
2018-10-25 13:04:21 +0900

    ディレクトリ削除、ログファイル名変更

--- test.ps1
+++ test.ps1
@@ -1,5 +1,7 @@
-﻿set LOG_PATH "./Expandlog.log" -option constant
+﻿set LOG_FILE "Expandlog.log" -option constant
 set DOWNLOAD_URI "http://sofme.unitech.jp/static/gameregister/file" -option constant
+set DOWNLOAD_FILE "sofme.unitech.jp/static/gameregister/file" -option constant
+set DOWNLOAD_DOMAIN "sofme.unitech.jp" -option constant
 
 function Log($text, $Level){
     $time = (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss.fff')
@@ -24,22 +26,30 @@ function CheckDataBase(){
         Log "DBが存在しません" 2
     } 
 }
-Start-Transcript -path $LOG_PATH -append;
+$date = (Get-Date -Format "yyyy-MM-dd")
+Start-Transcript -path "${date}-${LOG_FILE}" -append;
 
 $basedir = (Convert-Path ../);
 
 CheckDataBase;
 
 Log "ランチャールートディレクトリ：${basedir}"
-$gomi_zip = $basedir + "\gomi.zip";
-$gomi_index = $basedir + "\gomi.index";
+$gomi_zip = $basedir + "/gomi_zip";
+$gomi_index = $basedir + "/gomi_index";
 
 Log "ディスク作成:${gomi_zip}"
 $buff =  New-Item $gomi_zip -ItemType Directory -Force;
 Log "ディスク作成:${gomi_index}"
 $buff =  New-Item $gomi_index -ItemType Directory -Force;
 
-.\bin\wget.exe -r $DOWNLOAD_URI
+Log "データダウンロード"
+#./bin/wget.exe -r $DOWNLOAD_URI
+#Move-Item $DOWNLOAD_FILE "../game";
+
+#ゴミファイル削除
+Get-ChildItem $basedir -include  *.html* -r | ForEach-Object -Process { Remove-Item $_ | Log "削除:${_}"  }
+Log "ダウンロードディレクトリ削除:${basedir}/bin/${DOWNLOAD_DOMAIN}"
+Remove-Item "${basedir}/bin/${DOWNLOAD_DOMAIN}"
 
 Log "Search zip files..."
 $zipfiles = Get-ChildItem $basedir -Recurse | Where-Object {$_.Extension -eq ".zip"}
