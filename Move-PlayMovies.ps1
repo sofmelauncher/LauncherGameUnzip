91c1df4bd85c3d1015a5c89550748c1db188e61f
すー <aaaiiiuuu35@gmail.com>
2019-05-04 22:35:23 +0900

    fix

--- Move-PlayMovies.ps1
+++ Move-PlayMovies.ps1
@@ -4,10 +4,9 @@
 Set-StrictMode -Off
 Set-Variable PLAY_MOVIE_DIR "playmovie" -option constant
 if ($GAME_DIR -eq $null) {
-}
-if ($LOG_FILE -eq $null) {
     Set-Variable GAME_DIR "file" -option constant
 }
+
 Set-Variable Mo_LOG_FILE "Movie_Expandlog.log" -option constant
 
 function MovePlyaMovie {
