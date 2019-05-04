2eb1454bdf0b05b014235894b24e192d55b9a564
すー <aaaiiiuuu35@gmail.com>
2019-05-04 22:17:08 +0900

    update

--- Move-PlayMovies.ps1
+++ Move-PlayMovies.ps1
@@ -1,18 +1,19 @@
 ."./Log.ps1"
+."./Move-StrictItemWithDirectoryStructure.ps1"
 
 Set-StrictMode -Off
-Set-Variable PLAY_MOVIE_DIR "playMovie" -option constant
-if ($GAME_DIR -ne $null) {
-    Set-Variable GAME_DIR "file" -option constant
+Set-Variable PLAY_MOVIE_DIR "playmovie" -option constant
+if ($GAME_DIR -eq $null) {
 }
-if ($LOG_FILE -ne $null) {
-    Set-Variable LOG_FILE "Expandlog.log" -option constant
+Set-Variable GAME_DIR "file" -option constant
+if ($LOG_FILE -eq $null) {
 }
-
-$basedir = (Convert-Path ../);
-$date = (Get-Date -Format "yyyy-MM-dd")
+Set-Variable LOG_FILE "Expandlog.log" -option constant
 
 function MovePlyaMovie {
+    $basedir = (Convert-Path ../);
+    $date = (Get-Date -Format "yyyy-MM-dd")
+
     Start-Transcript -path "${basedir}\${date}-${LOG_FILE}" -append;
 
     #プレイ動画の移動
@@ -37,4 +38,4 @@ function MovePlyaMovie {
     }
     Stop-Transcript  
 }
-  
+MovePlyaMovie
