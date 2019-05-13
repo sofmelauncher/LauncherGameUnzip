c9bed69d8318b3569f0bca49f6fe48d34a76849b
すー <aaaiiiuuu35@gmail.com>
2019-05-13 18:26:17 +0900

    add exe list

---
+++ ExeList.ps1
@@ -0,0 +1,22 @@
+﻿."./Log.ps1"
+
+$basedir = (Convert-Path ../);
+
+$date = (Get-Date -Format "yyyy-MM-dd")
+
+#bin/ゲームの存在確認
+$is_expanded = Test-Path "${basedir}\${GAME_DIR}"
+Log "${basedir}\${GAME_DIR}の確認：${is_expanded}"
+
+if ($is_expanded -eq 1) {
+    Log "zipファイル探索ディレクトリ：${basedir}\Games"
+    $exefiles = Get-ChildItem "${basedir}\Games" -Recurse | Where-Object { $_.Extension -eq ".exe" }
+    
+    foreach ($item in $exefiles) {
+        TextLog "${item}"
+    }
+}
+else {
+    Log "データなし。"
+}
+
